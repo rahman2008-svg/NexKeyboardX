d78fcc71275b9c5c0ebe6c7e4be755030826d519
Abdur Rahman <prince.ar.abdur.rahman2008@gmail.com>
2026-06-12 16:45:40 +0600

    Update FoldStateProvider.kt

--- common/src/org/futo/inputmethod/latin/FoldStateProvider.kt
+++ common/src/org/futo/inputmethod/latin/FoldStateProvider.kt
@@ -1,11 +1,20 @@
-package org.futo.inputmethod.latin
+package com.nexvora.keyboard.latin
 
 import androidx.window.layout.FoldingFeature
 
+/**
+ * Represents folding-related device configuration for foldable devices.
+ * This helps the keyboard adapt its layout dynamically based on fold state.
+ */
 data class FoldingOptions(
     val feature: FoldingFeature?
 )
 
+/**
+ * Provides current fold state information of the device.
+ * Implementations should supply real-time folding updates
+ * for adaptive keyboard layout rendering.
+ */
 interface FoldStateProvider {
     val foldState: FoldingOptions
 }
