7f13e837f551c4df3332f01f3eadf69447bb69f1
Abdur Rahman <prince.ar.abdur.rahman2008@gmail.com>
2026-06-12 17:00:31 +0600

    Update ResizableIntArray.java

--- common/src/org/futo/inputmethod/latin/common/ResizableIntArray.java
+++ common/src/org/futo/inputmethod/latin/common/ResizableIntArray.java
@@ -1,19 +1,3 @@
-/*
- * Copyright (C) 2012 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package org.futo.inputmethod.latin.common;
 
 import org.futo.inputmethod.annotations.UsedForTesting;
@@ -22,59 +6,65 @@ import java.util.Arrays;
 
 import javax.annotation.Nonnull;
 
-// TODO: This class is not thread-safe.
+/**
+ * A dynamically resizable int array.
+ *
+ * NOTE: This class is NOT thread-safe.
+ */
 public final class ResizableIntArray {
+
     @Nonnull
     private int[] mArray;
+
     private int mLength;
 
-    public ResizableIntArray(final int capacity) {
-        reset(capacity);
+    public ResizableIntArray(int capacity) {
+        if (capacity < 0) capacity = 0;
+        mArray = new int[capacity];
+        mLength = 0;
     }
 
-    public int get(final int index) {
+    public int get(int index) {
+        if (index < 0 || index >= mLength) {
+            throw new ArrayIndexOutOfBoundsException(
+                    "length=" + mLength + ", index=" + index
+            );
+        }
+        return mArray[index];
+    }
+
+    public void addAt(int index, int value) {
+        if (index < 0) return;
+
         if (index < mLength) {
-            return mArray[index];
+            mArray[index] = value;
+            return;
         }
-        throw new ArrayIndexOutOfBoundsException("length=" + mLength + "; index=" + index);
+
+        ensureCapacity(index + 1);
+        mLength = index;
+        add(value);
     }
 
-    public void addAt(final int index, final int val) {
-        if (index < mLength) {
-            mArray[index] = val;
-        } else {
-            mLength = index;
-            add(val);
-        }
+    public void add(int value) {
+        ensureCapacity(mLength + 1);
+        mArray[mLength++] = value;
     }
 
-    public void add(final int val) {
-        final int currentLength = mLength;
-        ensureCapacity(currentLength + 1);
-        mArray[currentLength] = val;
-        mLength = currentLength + 1;
+    private int calculateCapacity(int minimumCapacity) {
+        int current = mArray.length;
+
+        if (current >= minimumCapacity) return 0;
+
+        int doubled = current == 0 ? 1 : current * 2;
+
+        return Math.max(minimumCapacity, doubled);
     }
 
-    /**
-     * Calculate the new capacity of {@code mArray}.
-     * @param minimumCapacity the minimum capacity that the {@code mArray} should have.
-     * @return the new capacity that the {@code mArray} should have. Returns zero when there is no
-     * need to expand {@code mArray}.
-     */
-    private int calculateCapacity(final int minimumCapacity) {
-        final int currentCapcity = mArray.length;
-        if (currentCapcity < minimumCapacity) {
-            final int nextCapacity = currentCapcity * 2;
-            // The following is the same as return Math.max(minimumCapacity, nextCapacity);
-            return minimumCapacity > nextCapacity ? minimumCapacity : nextCapacity;
-        }
-        return 0;
-    }
+    private void ensureCapacity(int minimumCapacity) {
+        int newCapacity = calculateCapacity(minimumCapacity);
 
-    private void ensureCapacity(final int minimumCapacity) {
-        final int newCapacity = calculateCapacity(minimumCapacity);
         if (newCapacity > 0) {
-            // TODO: Implement primitive array pool.
             mArray = Arrays.copyOf(mArray, newCapacity);
         }
     }
@@ -83,13 +73,20 @@ public final class ResizableIntArray {
         return mLength;
     }
 
-    public void setLength(final int newLength) {
+    public void setLength(int newLength) {
+        if (newLength < 0) newLength = 0;
+
         ensureCapacity(newLength);
+
+        if (newLength > mLength) {
+            Arrays.fill(mArray, mLength, newLength, 0);
+        }
+
         mLength = newLength;
     }
 
-    public void reset(final int capacity) {
-        // TODO: Implement primitive array pool.
+    public void reset(int capacity) {
+        if (capacity < 0) capacity = 0;
         mArray = new int[capacity];
         mLength = 0;
     }
@@ -99,64 +96,67 @@ public final class ResizableIntArray {
         return mArray;
     }
 
-    public void set(@Nonnull final ResizableIntArray ip) {
-        // TODO: Implement primitive array pool.
-        mArray = ip.mArray;
-        mLength = ip.mLength;
+    public void set(@Nonnull ResizableIntArray src) {
+        mArray = src.mArray;
+        mLength = src.mLength;
     }
 
-    public void copy(@Nonnull final ResizableIntArray ip) {
-        final int newCapacity = calculateCapacity(ip.mLength);
-        if (newCapacity > 0) {
-            // TODO: Implement primitive array pool.
-            mArray = new int[newCapacity];
+    public void copy(@Nonnull ResizableIntArray src) {
+        if (mArray.length < src.mLength) {
+            mArray = new int[src.mLength];
         }
-        System.arraycopy(ip.mArray, 0, mArray, 0, ip.mLength);
-        mLength = ip.mLength;
+
+        System.arraycopy(src.mArray, 0, mArray, 0, src.mLength);
+        mLength = src.mLength;
     }
 
-    public void append(@Nonnull final ResizableIntArray src, final int startPos, final int length) {
-        if (length == 0) {
-            return;
-        }
-        final int currentLength = mLength;
-        final int newLength = currentLength + length;
+    public void append(@Nonnull ResizableIntArray src, int startPos, int length) {
+        if (length <= 0) return;
+        if (startPos < 0 || startPos + length > src.mLength) return;
+
+        int newLength = mLength + length;
         ensureCapacity(newLength);
-        System.arraycopy(src.mArray, startPos, mArray, currentLength, length);
+
+        System.arraycopy(src.mArray, startPos, mArray, mLength, length);
         mLength = newLength;
     }
 
-    public void fill(final int value, final int startPos, final int length) {
-        if (startPos < 0 || length < 0) {
-            throw new IllegalArgumentException("startPos=" + startPos + "; length=" + length);
-        }
-        final int endPos = startPos + length;
-        ensureCapacity(endPos);
-        Arrays.fill(mArray, startPos, endPos, value);
-        if (mLength < endPos) {
-            mLength = endPos;
+    public void fill(int value, int startPos, int length) {
+        if (startPos < 0 || length <= 0) return;
+
+        int end = startPos + length;
+        ensureCapacity(end);
+
+        Arrays.fill(mArray, startPos, end, value);
+
+        if (mLength < end) {
+            mLength = end;
         }
     }
 
-    /**
-     * Shift to the left by elementCount, discarding elementCount pointers at the start.
-     * @param elementCount how many elements to shift.
-     */
     @UsedForTesting
-    public void shift(final int elementCount) {
+    public void shift(int elementCount) {
+        if (elementCount <= 0) return;
+        if (elementCount >= mLength) {
+            mLength = 0;
+            return;
+        }
+
         System.arraycopy(mArray, elementCount, mArray, 0, mLength - elementCount);
         mLength -= elementCount;
     }
 
     @Override
     public String toString() {
-        final StringBuilder sb = new StringBuilder();
+        StringBuilder sb = new StringBuilder();
+
+        sb.append('[');
         for (int i = 0; i < mLength; i++) {
-            if (i != 0) {
-                sb.append(",");
-            }
+            if (i > 0) sb.append(',');
             sb.append(mArray[i]);
         }
-        return "[" + sb + "]";
+        sb.append(']');
+
+        return sb.toString();
     }
 }
