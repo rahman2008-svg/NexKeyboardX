ce3bee32d6a45e98c9709a01e70ecd3d4005d11d
Abdur Rahman <prince.ar.abdur.rahman2008@gmail.com>
2026-06-12 16:44:39 +0600

    Update UsedForTesting.java

--- common/src/org/futo/inputmethod/annotations/UsedForTesting.java
+++ common/src/org/futo/inputmethod/annotations/UsedForTesting.java
@@ -1,24 +1,12 @@
-/*
- * Copyright (C) 2012 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-package org.futo.inputmethod.annotations;
+package com.nexvora.keyboard.annotations;
 
 /**
- * Denotes that the class, method or field should not be eliminated by ProGuard,
- * so that unit tests can access it. (See proguard.flags)
+ * Indicates that the annotated class, method, or field is intended
+ * to be accessible during testing and must not be removed or
+ * obfuscated by ProGuard/R8.
+ *
+ * This ensures unit tests and instrumentation tests can reliably
+ * access internal implementation details when required.
  */
 public @interface UsedForTesting {
 }
