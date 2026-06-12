7bbc0f295c638d48b58d893d41a9d27b2f5f4158
Abdur Rahman <prince.ar.abdur.rahman2008@gmail.com>
2026-06-12 16:43:44 +0600

    Update ExternallyReferenced.java

--- common/src/org/futo/inputmethod/annotations/ExternallyReferenced.java
+++ common/src/org/futo/inputmethod/annotations/ExternallyReferenced.java
@@ -1,24 +1,16 @@
-/*
- * Copyright (C) 2012 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-package org.futo.inputmethod.annotations;
+package com.nexvora.keyboard.annotations;
 
 /**
- * Denotes that the class, method or field should not be eliminated by ProGuard,
- * because it is externally referenced. (See proguard.flags)
+ * Indicates that the annotated class, method, or field must not be removed
+ * or obfuscated by ProGuard/R8 during build optimization.
+ *
+ * This is required for components that are accessed via:
+ * - Reflection
+ * - Native (JNI) code
+ * - Android system callbacks (e.g., InputMethodService)
+ * - XML or manifest declarations
+ *
+ * Removing these elements may break core keyboard functionality.
  */
 public @interface ExternallyReferenced {
 }
